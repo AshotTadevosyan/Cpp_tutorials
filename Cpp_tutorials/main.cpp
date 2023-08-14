077c8c3c86b6f6e3d25fba4627a6f99f81033824
Ashot Tadevosyan <ashottadevosyan27@gmail.com>
2023-08-14 14:13:41 +0400

    Initial Commit

---
+++ Cpp_tutorials/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp_tutorials
+//
+//  Created by Ashot Tadevosyan on 14.08.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
