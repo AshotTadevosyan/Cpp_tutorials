83b7d51c942230c17a953a5bb2a84ed7cf209d7b
Ashot Tadevosyan <ashottadevosyan27@gmail.com>
2023-08-18 10:30:23 +0400

    Problem 1

---
+++ Cpp_tutorials/print-that.hpp
@@ -0,0 +1,14 @@
+//
+//  print-that.hpp
+//  Cpp_tutorials
+//
+//  Created by Ashot Tadevosyan on 17.08.23.
+//
+#ifndef print_that_hpp
+#define print_that_hpp
+
+#include <string_view>
+
+void print_that(std::string_view string);
+
+#endif /* print_that_hpp */
